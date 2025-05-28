2435a8bbe26b3864fe2de009fdf06e0cb7c54876
jagadeeshu007 <100079151+jagadeeshu007@users.noreply.github.com>
2025-05-28 11:06:31 +0530

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -20,7 +20,7 @@ function Login() {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch("https://one0x-backend-1.onrender.com/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
