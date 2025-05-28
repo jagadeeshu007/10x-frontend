3c045e6295f306b9a72f5b0f94ff1d7b999c8379
Jagadeesh <jagadeeshu007@gmail.com>
2025-05-28 10:04:32 +0530

    login updated

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "./Login.css";
+import "./login.css";
 
 function Login() {
   const [userId, setUserId] = useState("");
