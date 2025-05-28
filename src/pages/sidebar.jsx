5dfcbb9dcd737883b78a4a3edcd443e39a16240e
jagadeeshu007 <100079151+jagadeeshu007@users.noreply.github.com>
2025-05-28 10:07:01 +0530

    Update sidebar.jsx

--- src/pages/sidebar.jsx
+++ src/pages/sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./Sidebar.css";
+import "./sidebar.css";
 
 function Sidebar() {
   const navigate = useNavigate();
